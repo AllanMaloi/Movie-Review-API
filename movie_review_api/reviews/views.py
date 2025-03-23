995698e40fab71619ad8914331ccbdc27778d5c7
AllanMaloi <maloisaruni@gmail.com>
2025-03-23 20:18:28 +0300

    made corrections to reviews/views.py and reviews/urls.py

--- movie_review_api/reviews/views.py
+++ movie_review_api/reviews/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, filters
 from .models import Review
 from .serializers import ReviewSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import IsOwnerOrReadOnly 
 from django_filters.rest_framework import DjangoFilterBackend
 
 class ReviewListCreateView(generics.ListCreateAPIView):
@@ -11,8 +12,8 @@ class ReviewListCreateView(generics.ListCreateAPIView):
 
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['movie__title', 'rating']
-    ordering_fields = ['rating', 'created_at']  # Allows sorting by rating & date
-    ordering = ['-created_at']  # Default: newest reviews first
+    ordering_fields = ['rating', 'created_at']  
+    ordering = ['-created_at']  
 
 class MovieReviewListView(generics.ListAPIView):
     serializer_class = ReviewSerializer
@@ -20,3 +21,9 @@ class MovieReviewListView(generics.ListAPIView):
     def get_queryset(self):
         movie_id = self.kwargs['movie_id']
         return Review.objects.filter(movie__id=movie_id)
+    
+class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
