19157dfe865de7d7a6cc38aa3c2020c23e1cfed5
AllanMaloi <maloisaruni@gmail.com>
2025-03-23 22:55:53 +0300

    fixed an error in urls.py in movie_review_api that led to an error that saidfor the base URL (http://127.0.0.1:8000/)

--- movie_review_api/movie_review_api/urls.py
+++ movie_review_api/movie_review_api/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse  
+
+def home(request):
+    return JsonResponse({"message": "Welcome to the Movie Review API!"})
 
 urlpatterns = [
+    path('', home),  
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/movies/', include('movies.urls')),
