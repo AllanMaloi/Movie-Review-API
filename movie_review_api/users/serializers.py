0387e4ed7fc628f71ebb62e8b231e3184a2b0314
AllanMaloi <maloisaruni@gmail.com>
2025-03-26 17:39:09 +0300

    edited serializers.py/users to ensures password isn't returned in responses

--- movie_review_api/users/serializers.py
+++ movie_review_api/users/serializers.py
@@ -4,11 +4,17 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)  
+
     class Meta:
         model = User
-        fields = ['id', 'email', 'username', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['id', 'username', 'email', 'password']
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password']) 
+        user.save()
         return user
