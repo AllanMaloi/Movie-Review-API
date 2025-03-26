bc4b6c3366453848a023372b3fca659c597f6317
AllanMaloi <maloisaruni@gmail.com>
2025-03-26 17:54:01 +0300

    edited views.py/users to restructure it to allow LoginView to use LoginSerializer for cleaner authentication and better validation.

--- movie_review_api/users/views.py
+++ movie_review_api/users/views.py
@@ -1,24 +1,20 @@
 from rest_framework.response import Response
 from rest_framework import status, generics
-from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import CustomUser
-from .serializers import UserSerializer
+from .serializers import UserSerializer, LoginSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
 
 class LoginView(generics.GenericAPIView):
-    serializer_class = UserSerializer
+    serializer_class = LoginSerializer
 
     def post(self, request, *args, **kwargs):
-        email = request.data.get("email")
-        password = request.data.get("password")
-        user = authenticate(email=email, password=password)
-
-        if not user:
-            return Response({"error": "Invalid email or password"}, status=status.HTTP_401_UNAUTHORIZED)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data  
 
         refresh = RefreshToken.for_user(user)
         return Response({
