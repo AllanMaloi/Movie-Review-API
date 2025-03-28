a3ca109798dde7019572b732765284f2acaeae0a
AllanMaloi <maloisaruni@gmail.com>
2025-03-28 18:47:54 +0300

    edited serializers.py/reviews to add validation of movie ratings

--- movie_review_api/reviews/serializers.py
+++ movie_review_api/reviews/serializers.py
@@ -6,3 +6,11 @@ class ReviewSerializer(serializers.ModelSerializer):
         model = Review
         fields = '__all__'
         extra_kwargs = {'user': {'read_only': True}}
+
+    
+    def validate_rating(self, value):
+        
+        if value < 1 or value > 5:
+            raise serializers.ValidationError("Rating must be between 1 and 5.")
+        return value
+
