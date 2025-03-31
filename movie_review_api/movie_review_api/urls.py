a1975097ab2bcea08e6a10a4cdfb600c216c5925
AllanMaloi <maloisaruni@gmail.com>
2025-03-31 16:21:04 +0300

    added JWT authentication tokens

--- movie_review_api/movie_review_api/urls.py
+++ movie_review_api/movie_review_api/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse  
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 def home(request):
     return JsonResponse({"message": "Welcome to the Movie Review API!"})
@@ -27,4 +28,8 @@ urlpatterns = [
     path('api/users/', include('users.urls')),
     path('api/movies/', include('movies.urls')),
     path('api/reviews/', include('reviews.urls')),
+
+    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
